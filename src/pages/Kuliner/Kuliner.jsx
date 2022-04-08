26489c039c4c9f34ac7f2c3dc4cbe084336757a8
Rozandi1 <98866052+Rozandi1@users.noreply.github.com>
2022-04-08 12:21:25 +0800

    page kuliner

--- src/pages/Kuliner/Kuliner.jsx
+++ src/pages/Kuliner/Kuliner.jsx
@@ -4,6 +4,7 @@ import CardProduct from "../../component/kuliner/cardProduct";
 import CartListItem from "../../component/kuliner/carListItem";
 import menu from "./kuliner-data";
 import { useEffect, useState } from "react";
+import Default from "../../layout/Default";
 
 export default function Kuliner() {
   const [total, setTotal] = useState(0);
@@ -62,57 +63,58 @@ export default function Kuliner() {
   }, [cart]);
   return (
     <>
-      <Navbar totalItem={purchasedItem} />
-      <div className="container">
-        <div className="pt-2 text-start">
-          <h3 className="display-7 fw-bold">Makanan Khas Pulau Sumbawa</h3>
-          <div className="col-xl-12 col-lg-6 mx-auto">
-            <p className="lead mb-3">Destinasi rekomendasi dan hal yang unik di pulau Sumbawa</p>
+      <Default totalItem={purchasedItem}>
+        <div className="container">
+          <div className="pt-2 text-start">
+            <h3 className="display-7 fw-bold">Makanan Khas Pulau Sumbawa</h3>
+            <div className="col-xl-12 col-lg-6 mx-auto">
+              <p className="lead mb-3">Destinasi rekomendasi dan hal yang unik di pulau Sumbawa</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="container">
-        <form className="d-flex">
-          <input className="form-control me-5 rounded" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success" type="submit">
-            Search
-          </button>
-        </form>
-      </div>
+        <div className="container">
+          <form className="d-flex">
+            <input className="form-control me-5 rounded" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
 
-      <div className="container py-5">
-        <div className="row">
-          <div className="col-md-8">
-            <div className="card w-100">
-              <div className="card-body">
-                <div className="row">
-                  {menu.map((menu) => (
-                    <div key={menu.id} className="col-md-4 col-sm-6 col-12 my-2">
-                      <CardProduct {...menu} addToCart={() => addToCart(menu.id)} />
-                    </div>
-                  ))}
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-md-8">
+              <div className="card w-100">
+                <div className="card-body">
+                  <div className="row">
+                    {menu.map((menu) => (
+                      <div key={menu.id} className="col-md-4 col-sm-6 col-12 my-2">
+                        <CardProduct {...menu} addToCart={() => addToCart(menu.id)} />
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col-md-4">
-            <ol className="list-group">
-              {cart.map((c) => {
-                return <CartListItem key={c.id} {...c} increase={() => increaseCartAmount(c.id)} decrease={() => decreaseCartAmount(c.id)} />;
-              })}
-              <li className="list-group-item d-flex justify-content-between align-items-start">
-                <div className="ms-2 me-auto">
-                  <div className="fw-bold">Total Harga</div>
-                </div>
-                {new Intl.NumberFormat("en-US", {
-                  style: "currency",
-                  currency: "USD",
-                }).format(total)}
-              </li>
-            </ol>
+            <div className="col-md-4">
+              <ol className="list-group">
+                {cart.map((c) => {
+                  return <CartListItem key={c.id} {...c} increase={() => increaseCartAmount(c.id)} decrease={() => decreaseCartAmount(c.id)} />;
+                })}
+                <li className="list-group-item d-flex justify-content-between align-items-start">
+                  <div className="ms-2 me-auto">
+                    <div className="fw-bold">Total Harga</div>
+                  </div>
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  }).format(total)}
+                </li>
+              </ol>
+            </div>
           </div>
         </div>
-      </div>
+      </Default>
     </>
   );
 }
